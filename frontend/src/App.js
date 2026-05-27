4de3692c95e736afc774d637c7957b4281d91397
Nayak007h <badhyanaik6@gmail.com>
2026-05-27 11:18:53 +0530

    Completed Dockerized DevOps project setup

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+
+function App() {
+
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://backend:5000/')
+      .then(response => response.text())
+      .then(data => setMessage(data));
+  }, []);
+
+  return (
+    <div style={{textAlign:'center', marginTop:'100px'}}>
+      <h1>CloudDeployX</h1>
+      <h2>{message}</h2>
+    </div>
+  );
+}
+
+export default App;
